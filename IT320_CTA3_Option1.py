8737a6d87ce56706ab90ad169ab412b82c69671d
feltmanerin4 <feltmanerin4@gmail.com>
2025-02-27 14:32:17 -0500

    Add files via upload

---
+++ IT320_CTA3_Option1.py
@@ -0,0 +1,32 @@
+#year_1962_to_1964 = $18,500
+#year_1965_to_1968 = $6,000
+#year_1968_to_1971 = $12,000
+#year_1972_to_1975 = $48,000
+#year_1976_to_1980 = $200,000
+#year_1981_to_1985 = $650,000
+#year_1986_to_2012 = $35,000,000
+#year_2013_to_2014 = $52,000,000
+value = ""
+
+
+year = int(input('Enter the year of car : '))
+
+
+if year in range(1962, 1964):
+    print('$18,500')
+elif year in range(1965, 1968):
+    print = '$6,000'
+elif year in range (1969,1971):
+    print = '$12,000'
+elif year in range (1972, 1975):
+    print = '$48,000'
+elif year in range (1976, 1980):
+    print =='$200,000'
+elif year in range (1981, 1985):
+    print = '$650,000'
+elif year in range (1986, 2012):
+    print = '$35,000,000'
+else:
+    year in range (2013, 2014)
+    print = '$52,000,000'
+    
